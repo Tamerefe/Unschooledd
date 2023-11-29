d0e6ed9dc6d85ae3947b4b9c3bada95ec7255c89
Tamerefe <tamerakipek@gmail.com>
2023-11-30 00:46:23 +0200

    Initial commit

---
+++ Waitlist/index.php
@@ -0,0 +1,40 @@
+<!Doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="style.css">
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
+    <title>Unschooledd</title>
+  </head>
+  <body>
+    <div class="card bg-dark text-white" style="position: fixed;">
+        <img src="Kiz.jpg" class="card-img" alt="Stony Beach"/>
+        <div class="card-img-overlay">
+            <div class="gnrl">
+                <h5 class="card-title wait">Join Our Waitlist!</h5>
+                <form class="form" method="post" action="mail.php">
+                    <input class="inpt" for="email" type="email" name="email" placeholder="E-mail" required>
+                    <input class="formbtn" type="submit" value="Enter">
+                </form>      
+            </div>
+        <img class="trn" src="turunc.png" alt="">
+        </div>
+      </div>
+
+    <!-- Optional JavaScript; choose one of the two! -->
+
+    <!-- Option 1: Bootstrap Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+
+    <!-- Option 2: Separate Popper and Bootstrap JS -->
+    <!--
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
+    -->
+  </body>
+</html>
+
---
+++ Web/lecture.js
@@ -0,0 +1,12 @@
+function openinsta() {
+    window.open("https://www.instagram.com/unschooleddd/", "_blank");
+}
+function openlinkd() {
+    window.open("https://www.linkedin.com/company/unschooledd/", "_blank");
+}
+function openytbe() {
+    window.open("https://www.youtube.com/", "_blank");
+}
+function opentwit() {
+    window.open("https://www.twitter.com/", "_blank");
+}
